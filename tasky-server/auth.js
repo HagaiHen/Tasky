1fa42a2a4f7d7f2bf11392faf88a93f49dffdd0e
eladsez <eladsez36@gmail.com>
2023-04-05 11:06:12 +0300

    server auth part

--- tasky-server/auth.js
+++ tasky-server/auth.js
@@ -5,7 +5,7 @@ import admin from "firebase-admin"
 admin.initializeApp({
     Credential: admin.credential.cert(firebaseadmin),
 });
-const auth = admin.auth();
+const firebaseAuth = admin.auth();
 
 
 const app = express();
@@ -19,12 +19,24 @@ app.post("/signup", async (req, res) => {
         email: req.body.email,
         password: req.body.password,
     }
-    const userResponse = await auth.createUser({
+    const userResponse = await firebaseAuth.createUser({
         email: user.email,
         password: user.password,
         emailVerified: false,
         disabled: false,
     });
+    res.json(userResponse);
+});
+
+
+// need to fix this
+app.post("/login", async (req, res) => {
+    console.log(req.body);
+    const user = {
+        email: req.body.email,
+        password: req.body.password,
+    }
+    const userResponse = await firebaseAuth.sign(user.email);
     res.json(userResponse);
 });
 
