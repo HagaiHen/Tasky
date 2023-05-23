a614f31edc8d5828abd1b34d8c51d85501d5b622
eladsez <eladsez36@gmail.com>
2023-05-23 20:02:42 +0300

    fix fucking mess

--- tasky/next.config.js
+++ tasky/next.config.js
@@ -5,16 +5,16 @@ const nextConfig = {
 
 module.exports = nextConfig
 
-require('dotenv').config();
+// require('dotenv').config();
 
-module.exports = {
-  env: {
-    API_KEY: process.env.apiKey,
-    AUTH_DOMAIN: process.env.authDomain,
-    PROJECT_ID: process.env.projectId,
-    STORAGE_BUCKET: process.env.storageBucket,
-    MESSAGING_SENDER_ID: process.env.messagingSenderId,
-    APP_ID: process.env.appId,
-    SERVER: process.env.SERVER
-  }
-};
+// module.exports = {
+//   env: {
+//     API_KEY: process.env.apiKey,
+//     AUTH_DOMAIN: process.env.authDomain,
+//     PROJECT_ID: process.env.projectId,
+//     STORAGE_BUCKET: process.env.storageBucket,
+//     MESSAGING_SENDER_ID: process.env.messagingSenderId,
+//     APP_ID: process.env.appId,
+//     SERVER: process.env.SERVER
+//   }
+// };
--- tasky/src/pages/auth/signin.js
+++ tasky/src/pages/auth/signin.js
@@ -12,7 +12,7 @@ import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { signIn } from '../../utils/auth';
+import { signIn } from '../../utils/controller';
 import Router from 'next/router'
 
 function Copyright(props) {
