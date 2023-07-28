f8ce7332b1dfd63684b45ca2cc626f71ff4d9370
bar.goldenberg <bar.goldenberg@similarweb.com>
2023-07-28 14:06:48 +0300

    added changes

--- tasky/src/controller/TeamController.js
+++ tasky/src/controller/TeamController.js
@@ -1,31 +1,14 @@
 import { postMessage, getMessage } from "./APIController";
 
-export const createTask = async (
-  assignee,
-  buisnessValue,
-  dependencies,
-  description,
-  devEffort,
-  riskReduction,
-  urgency,
-  sprint,
-  status,
-  taskNum,
-  title
-) => {
-  await postMessage("/task/createTask", {
-    Assignee: assignee,
-    BuisnessValue: buisnessValue,
-    Dependencies: dependencies,
-    Description: description,
-    DevelopmentEffort: devEffort,
-    RiskReduction: riskReduction,
-    Urgency: urgency,
-    Sprint: sprint,
-    Status: status,
-    TaskNum: taskNum,
-    Title: title,
-  }).catch((err) => {
-    alert("couldnt create task");
+export const createTeam = async (id, name, teamLead, members, tasks) => {
+  await postMessage(
+    "/task/createTask",
+    new Team(name, teamLead, members, tasks)
+  ).catch((err) => {
+    alert("couldnt create team");
   });
+  const getTeam = async (id) => {
+    const team = await postMessage("/task/getTeam", id).catch((err) => alert(err));
+    return team;
+  };
 };
