fb220e5256f3a8763b136a1db67aa1c0a1582a54
eladsez <eladsez36@gmail.com>
2023-08-02 21:01:47 +0300

    recent project added

--- tasky/src/components/Header/Header.js
+++ tasky/src/components/Header/Header.js
@@ -23,37 +23,35 @@ const NavHeader = (props) => {
           src="./Logo.svg"
           style={{ marginLeft: "15px" }}
         />
-        {
-          headerButtons.map((button) => (
-            <NavButton
-              active={activePage === button}
-              onClick={() => handleNavigation(button)}
-            >
-              {button}
-            </NavButton>
-          ))
-        }
+        {headerButtons.map((button) => (
+          <NavButton
+            active={activePage === button}
+            onClick={() => handleNavigation(button)}
+          >
+            {button}
+          </NavButton>
+        ))}
         <Image
           width={70}
           height={38}
           src="./ProfileClicked.svg"
-          style={{ marginLeft: "46%", marginTop: "1.2%", cursor: "pointer" }}
+          style={{ marginLeft: "35%", marginTop: "1.2%", cursor: "pointer" }}
         />
       </HeaderContainer>
-      {(() => {
-        switch (activePage) {
-          case "Home":
-            return <Home />;
-          case "Calendar":
-            return <h1>Calendar Page</h1>;
-          case "Backlog":
-            return <Backlog />;
-          case "Profile":
-            return <h1>Profile Page</h1>;
-          default:
-            return <Home />;
-        }
-      })()}
+        {(() => {
+          switch (activePage) {
+            case "Home":
+              return <Home />;
+            case "Calendar":
+              return <h1>Calendar Page</h1>;
+            case "Backlog":
+              return <Backlog />;
+            case "Profile":
+              return <h1>Profile Page</h1>;
+            default:
+              return <Home />;
+          }
+        })()}
     </Container>
   );
 };
---
+++ tasky/src/pages/home/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const RecentProjectsContainer = styled.div`
+  background-color: #4b4747;
+  width: 100%;
+  height: 100%;
+  overflow-x: auto; /* Use "auto" instead of "scroll" for auto-scroll behavior */
+  scroll-behavior: smooth;
+  display: grid;
+  grid-auto-flow: column; /* Make it flow horizontally */
+`;
