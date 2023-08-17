206d84ede0189ba6bee5efb987f00469890eacef
Sappir Bohbot <sappir.bohbot@msmail.ariel.ac.il>
2023-08-17 13:01:58 +0300

    Fix ProgChart

--- tasky/src/components/ProgChart/ProgChart.js
+++ tasky/src/components/ProgChart/ProgChart.js
@@ -16,36 +16,46 @@ const ProgChart = (params) => {
   const [countToDo   , setCountToDo] = useState(0);
   const [countInProg, setcountInProg]= useState(0);
   const [countDone   , setCountDone] = useState(0);
+  const [countInReview, setcountInReview]= useState(0);
+  const [countInTest   , setcountInTest] = useState(0);
 
 
   
   useEffect(()=>{
-    var cBkLg = 0;
     var cToDo = 0;
+    var cBkLg = 0;
     var cInProg = 0;
+    var cInReview = 0;
+    var cInTest = 0;
     var cDone =0;
     const tasks = params.tasks;
 
     tasks.forEach(task => {
+      if(task.data.status == 0){cToDo += 1;}
       if(task.data.status == 1){cBkLg += 1;}
-      if(task.data.status == 2){cToDo += 1;}
-      if(task.data.status == 3){cInProg += 1;}
-      if(task.data.status == 4){cDone += 1;}
+      if(task.data.status == 2){cInProg += 1;}
+      if(task.data.status == 3){cInReview += 1;}
+      if(task.data.status == 4){cInTest += 1;}
+      if(task.data.status == 5){cDone += 1;}
     });
     
     setCountBacklog(cBkLg);
     setCountToDo(cToDo);
     setcountInProg(cInProg);
     setCountDone(cDone);
+    setcountInReview(cInReview);
+    setcountInTest(cInTest);
 
   }, [params.tasks]);
 
   
   
   const data = [
-    { country: 'Backlog', area: countBacklog },
     { country: 'ToDo', area: countToDo },
+    { country: 'Backlog', area: countBacklog },
     { country: 'In Progress', area: countInProg },
+    { country: 'In Review', area: countInReview },
+    { country: 'In Test', area: countInTest },
     { country: 'Done', area: countDone },
   ];
   const chartData = data;
